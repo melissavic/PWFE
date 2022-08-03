c6f67a91b5d21f3121df7e2f102c7e917b02fdd7
Melissa Victória <melzinhalacerdaaraujo@gmail.com>
2022-08-03 11:21:25 -0300

    validarCores

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 'use strict'
 
-function mudarCor () {
-    const cor = document.getElementById('cor').value
-    document.body.style.backgroundColor = `#${cor}`
-    document.documentElement.style.setProperty('--bg-color' , `#${cor}`)
-
-    decodeURIComponent.getElementById('aplicar').addEventListenner('click' , mudarCor)
+function mudarCor(){
+  
+    const cor = document.getElementById('cor').value.toUpperCase()
+    const body = document.getElementById('body')
+    body.style.backgroundColor = `#${cor}`
+    
+    
+    if ( cor == 'VERMELHO' ) {
+        body.style.backgroundColor = '#990000'
+    }
+    else if ( cor == 'AZUL' ) {
+        body.style.backgroundColor = '#00008B'
+    }
+    else if ( cor == 'AMARELO' ) {
+        body.style.backgroundColor = '#EEAD2D'
+    }
+    else if ( cor == 'VERDE' ) {
+        body.style.backgroundColor = '#006400'
+    }
+    else if (cor == 'ROSA') {
+        body.style.backgroundColor = '#FFB6C1'
+    }
+    else if (cor == 'ROXO') {
+        body.style.backgroundColor = '#800080'
+    }
+    else if (cor == 'CINZA') {
+        body.style.backgroundColor = '7f7f7f'
+    }
+    
 }
+
+const botao = document.getElementById('botao').addEventListener('click', mudarCor)
